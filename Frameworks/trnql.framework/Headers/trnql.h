9c610e57144c2a142b4001e10c28e29e895f2568
trnql <eng@trnql.com>
2015-12-02 11:52:51 -0800

    Version 1.5

---
+++ Frameworks/trnql.framework/Headers/trnql.h
@@ -0,0 +1,19 @@
+//
+//  trnql.h
+//  trnql
+//
+//  Created by Jonathan Sahoo on 8/12/15.
+//  Copyright (c) 2015 trnql. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for trnql.
+FOUNDATION_EXPORT double trnqlVersionNumber;
+
+//! Project version string for trnql.
+FOUNDATION_EXPORT const unsigned char trnqlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <trnql/PublicHeader.h>
+
+
